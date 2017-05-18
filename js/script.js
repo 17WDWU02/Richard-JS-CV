bd2290edccc40783bda320f13b36114a1d7cc9e2
Richard Hpa <richard.hpa@hotmail.com>
2017-05-19 11:53:08 +1200

    Added Progress Bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var overlay = false;
+
 var Education = ['Scots College Wellington',
  				'Victoria University of Wellington',
  				 'Yoobee School of Design'
@@ -20,6 +22,8 @@ var Employment = [
 	}
 ];
 
+
+
 var HeaderName = document.getElementById('Name');
 HeaderName.style.color = "red";
 HeaderName.style.fontSize = "50px";
@@ -41,8 +45,11 @@ for (var i = 0; i < Employment.length; i++) {
 var EmploymentBlock = document.getElementsByClassName('EmploymentItem');
 for (var i = 0; i < EmploymentBlock.length; i++) {
 	EmploymentBlock[i].onclick = function(){
-		var companyname = this.childNodes[0].innerText;
-		popup(companyname);
+		if(overlay === false){
+			var companyname = this.childNodes[0].innerText;
+			popup(companyname);
+			overlay = true;
+		}
 	}
 };
 
@@ -64,8 +71,74 @@ document.getElementById('Close').addEventListener("click", ClosePopUp);
 
 function ClosePopUp(){
 	// popupbox.style.display = 'none';
+	overlay = false;
 	document.getElementById('popup').style.display = "none";
 }
+
+
+
+
+
+
+var Skills = [
+	{
+		SkillName : "HTML",
+		Percentage : "80"
+	},
+	{
+		SkillName : "CSS",
+		Percentage : "70"
+	},
+	{
+		SkillName : "JS",
+		Percentage : "60"
+	}
+]
+
+//Skills Bar
+var SkillContainer = document.getElementById('SkillContainer');
+for (var i = 0; i < Skills.length; i++) {
+	SkillContainer.innerHTML += "<label>"+Skills[i]['SkillName']+"</label>"+
+								"<div class='SkillBar'>"+
+									"<div id='"+Skills[i].SkillName+"' class='progress'></div>"+
+								"</div>"
+};
+
+var bars = document.getElementsByClassName('progress');
+
+function progressSkills(){
+	for (var i = 0; i < bars.length; i++) {
+		var StopPoint;
+		for(var j = 0; j < Skills.length; j++){
+			if(bars[i].id == Skills[j]['SkillName']){
+				StopPoint = Skills[j]['Percentage'];
+				break;
+			}
+		}
+	move(bars[i], StopPoint);
+	}
+}
+
+function move(bar, StopPoint){
+	var width = 0;
+	var barMove = setInterval(barGrow, 10);
+	function barGrow(){
+		if(width >= StopPoint){
+			clearInterval(barMove);
+		} else {
+			width++;
+			bar.style.width = width + '%';
+		}
+	}
+}
+
+
+
+
+
+
+
+
 
 
 
